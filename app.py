86c1f56f2c5ce4ad648feb48e01c6f9fd06d2755
heejin22 <std.bigdata@gmail.com>
2024-11-27 10:17:48 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-
-# 페이지 타이틀과 아이콘을 지정할 수있다. 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
-
+# 웹페이지에 글자 출력하기
+st.title('hello')
+st.subheader('서브헤더')
+st.text('텍스트 출력')
+st.image('이미지url')
